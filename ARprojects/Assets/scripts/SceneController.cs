e467c7d604d564211a8d06d0e8792e9b84cb0f3f
VHMOliveira <Alquimia01>
2018-05-12 14:41:06 -0300

    atualização1
    atualização

---
+++ ARprojects/Assets/scripts/SceneController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour {
+
+    public Animator anim;
+
+    public void CommandActions(string Commands) {
+        Commands = Commands.Trim();
+
+        switch (Commands) {
+
+            case "running":
+                anim.SetBool("stop", false);
+                anim.Play("Run");
+                break;
+            case "jump":
+                anim.Play("Jump");
+                break;
+            case "stop":
+                anim.SetBool("stop",true);
+                break;
+            default:
+                anim.Play("Idle");
+                break;
+        }
+    }
+}
---
+++ ARprojects/Assets/scripts/changeColor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeColor : MonoBehaviour {
+
+    private GameObject target;
+    public float range;
+    //Declare a raycast hit to store information about what our raycast has hit
+    RaycastHit hit;
+
+	// Update is called once per frame
+	void Update () {
+        ChangeColor();
+	}
+
+    void ChangeColor() {
+        Vector3 origin = this.transform.position;
+        Debug.DrawRay(origin, Vector3.left, Color.green);
+            
+        if (Physics.Raycast(origin,Vector3.left,out hit,range)) {
+            MeshRenderer colorBase = hit.collider.GetComponent<MeshRenderer>();
+            colorBase.material.color = Color.red;
+        }
+    }
+}
